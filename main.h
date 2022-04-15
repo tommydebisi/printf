8d123df1d0713ffccb97de161a1e21240777b160
tobygal <tochiazolike@yahoo.com>
2022-04-15 11:32:13 -0700

    update main.h

--- main.h
+++ main.h
@@ -1,6 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int _putchar(char c);
+int _strlen(char *c);
+int _printf(const char *format, ...);
 
 #endif /*_MAIN_H_*/
