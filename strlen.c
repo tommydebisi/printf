3b122900fbf04727a20ec6b142db9ab54198c004
tobygal <tochiazolike@yahoo.com>
2022-04-15 11:43:27 -0700

    creation of strlen.c

---
+++ strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate the length of a string
+ * @c: string passed as argument
+ * Return: return length of string
+ */
+
+int _strlen(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
