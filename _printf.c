de2ae16c18be8927e58a3d4c081568142d1788d3
tommydebisi <tommydebisi@gmail.com>
2022-04-17 06:08:08 +0000

    task0 update2

--- _printf.c
+++ _printf.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to output
- * @format: argument
- * Return: res
+ * _printf - prints and returns length of format and arg.
+ * @format: string where specifiers are also called
+ * Return: count sum
  */
 
 int _printf(const char *format, ...)
@@ -13,36 +13,36 @@ int _printf(const char *format, ...)
 	va_list arg;
 	/* initialize arg with format*/
 	va_start(arg, format);
+	if (!format)
+		return (-1);
 	for (i = 0; format && format[i]; i++)
 	{
 		if (format[i] != '%')
-		{
+		{/*it is a normal character so print*/
 			_putchar(format[i]);
 			sum += 1;
 			continue;
 		}
 		if (format[i] == '%')
 		{
-			++i;
-			if (format[i] == '%')
+			if (format[i + 1] == '%')
 			{/*means it's %% so it should print %*/
 				_putchar('%');
 				sum += 1;
 				continue;
 			}
-			if (format[i] == '\0')
-				return (-1);
-			count = count_spec(format[i], arg);/*the length*/
 		}
-		if (count > 0)
-			sum += count;
-		if (count == 0 && format[++i] == '\0')
+		if (format[i + 1] == '\0')
 			return (-1);
-		if (count == 0)/*no stat. was done do this*/
+		count = count_spec(format[i + 1], arg);/*the count of arg*/
+		if (count == -1 || count != 0)
+			i++;
+		if (count > 0)/*count_spec used and the specifier is correct*/
+			sum += count;
+		if (count == 0)/*invalid specifier, treat as normal character*/
 		{
 			_putchar('%');
-			_putchar(format[i]);
-			sum += 2;
+			sum += 1;
 		}
 	}
 	va_end(arg);
